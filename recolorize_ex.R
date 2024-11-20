087dbd17556e8e0e3cfff8400cbcd5ed76686182
vincenzobusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2024-11-20 14:26:34 +0100

    Update recolorize_ex.R

--- recolorize_ex.R
+++ recolorize_ex.R
@@ -1,1 +1,13 @@
+library(recolorize)
 
+# get the path to the image (comes with the package, so we use system.file):
+img <- system.file("extdata/corbetti.png", package = "recolorize")
+
+# fit a color map (only provided parameter is a color similarity cutoff)
+recolorize_obj <- recolorize2(img, cutoff = 45)
+
+init_fit <- recolorize(img, method = "hist", bins = 2, color_space = "sRGB")
+#> 
+#> Using 2^3 = 8 total bins
+
+refined_fit <- recluster(init_fit, cutoff = 45)
