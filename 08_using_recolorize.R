f457beec6019560508555c5c60eee6d9f6567a92
vincenzobusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2025-01-22 12:37:09 +0100

    Update 08_using_recolorize.R

--- 08_using_recolorize.R
+++ 08_using_recolorize.R
@@ -1,51 +1,51 @@
 library(recolorize)
 
-# carico le immagini delle orchidee
+# uploading orchids images / carico le immagini delle orchidee
 catt <- system.file("extdata/cattleya.png", package = "recolorize")
 cymb <- system.file("extdata/cymbidium.png", package = "recolorize")
 milt <- system.file("extdata/miltoniopsis.png", package = "recolorize")
 rest <- system.file("extdata/restrepia.png", package = "recolorize")
 
-# fase iniziale di clustering
+# initial clustering phase / fase iniziale di clustering
 init_catt <- recolorize(catt, method = "hist", bins = 2, color_space = "sRGB")
 init_cymb <- recolorize(cymb, method = "hist", bins = 2, color_space = "sRGB")
 init_milt <- recolorize(milt, method = "hist", bins = 2, color_space = "sRGB")
 init_rest <- recolorize(rest, method = "hist", bins = 2, color_space = "sRGB")
 
-# fase di refining
+# refining phase / fase di refining
 ref_catt <- recluster(init_catt, cutoff = 45)
 ref_cymb <- recluster(init_cymb, cutoff = 45)
 ref_milt <- recluster(init_milt, cutoff = 45)
 ref_rest <- recluster(init_rest, cutoff = 45)
 
-# refining manuale
+# manual refining / refining manuale
 final_catt <- editLayer(ref_catt, 3, operation = "fill", px_size = 4)
-final_cymb <- editLayer(ref_cymb, 2, operation = "fill", px_size = 4) #parametro impostato a 2 per mancanza di layer
+final_cymb <- editLayer(ref_cymb, 2, operation = "fill", px_size = 4) #parameter is 2 for lack of layers / parametro impostato a 2 per mancanza di layer
 final_milt <- editLayer(ref_milt, 3, operation = "fill", px_size = 4)
-final_rest <- editLayer(ref_rest, 2, operation = "fill", px_size = 4) #parametro impostato a 2 per mancanza di layer
+final_rest <- editLayer(ref_rest, 2, operation = "fill", px_size = 4) #parameter is 2 for lack of layers / parametro impostato a 2 per mancanza di layer
 
 
 
-## splitByColor() di cattleya
+## splitByColor() cattleya
 
-    catt_rc <- recolorize2(catt, cutoff = 45, plotting = FALSE) # esegue recolorize() e recluster() in modo consecutivo
+    catt_rc <- recolorize2(catt, cutoff = 45, plotting = FALSE) # runs sequentially recolorize() and recluster() / esegue recolorize() e recluster() in modo consecutivo
 
-# impostazione della schermata di visualizzazione
+# layout setting / impostazione della schermata di visualizzazione
     layout(matrix(1:6, nrow = 2, byrow = TRUE))
     par(mar = c(0,0,2,0))
 
-    lay_catt <- splitByColor(catt_rc, plot_method = "overlay") # divisione della mappa di colore in singoli layer 
+    lay_catt <- splitByColor(catt_rc, plot_method = "overlay") # dividing the color map in single layers / divisione della mappa di colore in singoli layer 
 
     for (i in 1:length(lay_catt))
       {
           plotImageArray(lay_catt[[i]], main = i)
       }
 
-### esportazione dei livelli di colore
+### exporting color layers / esportazione dei livelli di colore
 
-    recolorize_to_png(catt_rc, filename = "catt_recolored.png") # crea un file .png dell'immagine ricolorata
+    recolorize_to_png(catt_rc, filename = "catt_recolored.png") # creating file .png of recolored image / crea un file .png dell'immagine ricolorata
 
-        for (i in 1:length(lay_catt)) # salva i singoli layer binari in formato .png
+        for (i in 1:length(lay_catt)) # saving single binary layers in .png / salva i singoli layer binari in formato .png
           {
               png::writePNG(lay_catt[[i]], target = paste0("cattlayer_", i, ".png"))
           }
@@ -53,7 +53,7 @@ final_rest <- editLayer(ref_rest, 2, operation = "fill", px_size = 4) #parametro
 
 
 
-## splitByColor() di cymbidium
+## splitByColor() cymbidium
 
     cymb_rc <- recolorize2(cymb, cutoff = 45, plotting = FALSE) # esegue recolorize() e recluster() in modo consecutivo
 
